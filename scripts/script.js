05e6cdd3540fbb210e25d621343810f32c6ed459
pattah <rosiak211@gmail.com>
2022-07-03 17:28:25 +0200

    finished project

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,18 @@
 
-const wrongDisplay = document.querySelectorAll('.wrongInfo');
+const wrongDisplay = document.querySelector('.wrongInfo');
 const formData = document.querySelector('.formData');
-const formInput = document.querySelectorAll('.formInput')
 const firstPassword = document.querySelector('#firstPassword')
-  const passwordConfirm = document.querySelector('#passwordConfirm')
+const passwordConfirm = document.querySelector('#passwordConfirm')
 const submitButton = document.querySelector('.submitButton')
 const dupa = document.querySelector('.firstNameCont')
+const formInput = document.querySelectorAll('.formInput')
 
 submitButton.addEventListener('click', () => {
   formInput.forEach(el => {
-       if(el.value === '') {
-            wrongDisplay.textContent = 'required field'
-       }
-  });
+  if (el.value === '') {
+    el.style.border = '1px solid red'
+  } else {
+    el.style.border = '1px solid rgb(170, 170, 170)';
+  }
 });
-
-
-
-
-
-
-
+});
